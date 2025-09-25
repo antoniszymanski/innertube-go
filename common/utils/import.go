c98f6f1bdf51d7b05574f74a5da91e7c3c9a3326
Antoni Szymański <antoni.szymanski+github@proton.me>
2025-09-26 00:24:43 +0200

    fix(utils): correct handling of nil input in ImportFrom

--- common/utils/import.go
+++ common/utils/import.go
@@ -3,19 +3,29 @@
 
 package utils
 
-import "github.com/dop251/goja"
+import (
+	"reflect"
+
+	"github.com/dop251/goja"
+)
 
 type ToValue interface {
 	ToValue(vm *goja.Runtime) (goja.Value, error)
 }
 
 func ImportFrom(vm *goja.Runtime, in any) (goja.Value, error) {
-	switch i := in.(type) {
-	case nil:
+	if in == nil || try(reflect.ValueOf(in).IsNil) {
 		return goja.Undefined(), nil
+	}
+	switch i := in.(type) {
 	case ToValue:
 		return i.ToValue(vm)
 	default:
 		return vm.ToValue(in), nil
 	}
+}
+
+func try[T any](f func() T) T {
+	defer func() { _ = recover() }()
+	return f()
 }
